593f0d7be85ace0f6871977ecf38d575bddf39f0
Muhamad Arif Farhan <dedearif2017@gmail.com>
2026-09-10 09:10:12 +0700

    First Commit

---
+++ app/src/main/java/com/example/activity_intent/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.activity_intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.activity_intent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivityLifecycle"
+    private lateinit var binding: ActivityMainBinding
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            btnToSecondActivity.setOnClickListener {
+                val intentToSecondActivity =
+                    Intent(this@MainActivity, SecondActivity::class.java)
+                startActivity(intentToSecondActivity)
+            }
+        }
+        Log.d(TAG, "onCreate: dipanggil")
+    }
+}
